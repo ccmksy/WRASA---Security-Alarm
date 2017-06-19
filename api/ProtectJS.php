264231610a1cb4f004263d85bfe0242b354c5c93
Mike Yeung <ccmksy@gmail.com>
2017-06-19 19:54:48 +0800

    Update ProtectJS.php

--- api/ProtectJS.php
+++ api/ProtectJS.php
@@ -10,7 +10,7 @@
  * @copyright	Copyright (c) 2016 - 2017, Cloud Design Limited (http://www.cloud-design.hk/)
  * @license	http://opensource.org/licenses/MIT	MIT License
  * @link	http://wrasa.codage.tech
- * @since	Version 1.0.0
+ * @version     1.0.1
  * @filesource
  */
 
